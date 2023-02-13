97a8085c50693a59b2a0fd193301f47a59c0d273
Zo Grant IV <zograntiv@gmail.com>
2023-02-13 13:43:15 -0600

    added js

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-let a = 1;
-
-function (x12) {
-let b = 5;
-console.log(a);
-console.log(b + a);
-}
-
-let lesson = function (x) {
-    let b = 2;
-
-    console.log(x + b);
-}
-
-lesson(a);
-12();
-
 function whodat() {
     let name = prompt("What is your name?")
     //let name = console.Readline();
