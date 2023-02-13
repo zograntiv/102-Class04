d166f2a624d4d6ffe45748067cf207a1129b8304
Zo Grant IV <zograntiv@gmail.com>
2023-02-13 06:18:18 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 let a = 1;
 
-let  lesson= function (x) {
-let b = 2;
+function (x12) {
+let b = 5;
+console.log(a);
+console.log(b + a);
+}
 
-console.log (x+b);
+let lesson = function (x) {
+    let b = 2;
+
+    console.log(x + b);
 }
 
 lesson(a);
+12();
 
-console.log(b);
+function whodat() {
+    let name = prompt("What is your name?")
+    //let name = console.Readline();
+    if (name != 'ZO') {
+        alert(ERROR);
+    } else {
+        alert("Hello," + name);
+    }
+}
+
+
+
+
+
+//console.log(b);
