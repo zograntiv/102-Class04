2659984977aa3a2872688a8c049602d0d78ba38d
Zo Grant IV <zograntiv@gmail.com>
2023-02-14 14:47:11 -0600

    Updated site
    added java

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-function whodat() {
-    let name = prompt("What is your name?")
-    //let name = console.Readline();
-    if (name != 'ZO') {
-        alert(ERROR);
-    } else {
-        alert("Hello," + name);
+
+
+
+function emailValidiation() {
+    let email= document.getElementById("eField").value;
+
+    if (email == "zograntiv@gmail.com") {
+        alert("correct email !!");
+
+    } 
+
+    if (email != "zograntiv@gmail.com)") {
+        alert("wrong email !!");
+
+
     }
+    // if (email.includes('@') && email.includes ('.')){
+    //     //true
+    // } else {
+        
+    // }
 }
-
-
 
 
 
